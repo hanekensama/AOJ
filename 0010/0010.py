5b174a1534acdb2077811712d81d5fe7d9e90344
hanekensama <is0182vp@ed.ritsumei.ac.jp>
2016-11-16 14:38:32 +0900

    0010 unsolved

---
+++ 0010/0010.py
@@ -0,0 +1,23 @@
+import math
+
+
+def dist(a, b):
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
+
+
+def R(a, b, c):
+    theta = math.acos((a * a + b * b - c * c) / 2 * a * b)
+    return (c / math.sin(theta)) / 2
+
+
+def main():
+    N = int(input())
+    for i in range(N):
+        inputs = list(map(float, input().split()))
+        p = [(x, y) for (x, y) in zip(inputs[::2], inputs[1::2])]
+
+        print(R(dist(p[0], p[1]), dist(p[1], p[2]), dist(p[2], p[0])))
+
+
+if __name__ == '__main__':
+    main()
